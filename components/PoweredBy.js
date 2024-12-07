72cb1dc233c90ef533ad6c03eb00083033f08a53
一条老鱼 <hainasjoy@gmail.com>
2024-12-07 14:31:25 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,13 +8,12 @@ export default function PoweredBy(props) {
   return (
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
-      <span>Powered by</span>
+      <span>Mr.</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://jiangnanxiaohe.com'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        JIANGNANXIAOHE {siteConfig('VERSION')}
       </a>
-      .
     </div>
   )
 }
